b551d8faee9b5753482058a349a00cb99f24d9f0
Stephane Gagnon <stephane-gagnon@hotmail.com>
2025-10-20 19:36:13 -0400

    refactor(control): Rename control variables for clarity and update references in control patch

--- src/includes/control.py
+++ src/includes/control.py
@@ -9,9 +9,9 @@ volume_filter  = CtrlFilter(7)  >> CtrlValueFilter(0, 101)
 trigger_filter = Filter(NOTEON) >> Transpose(-36)
 transport_filter = [jump_filter, volume_filter, trigger_filter]
 
-key_mp3_control = transport_filter >> MPG123_SD90_A
-pk5_mp3_control = transport_filter >> MPG123_SD90_B
-mpk_vlc_control = trigger_filter >> VLC_BASE
+mpg123_control_1 = transport_filter >> MPG123_SD90_A
+mpg123_control_2 = transport_filter >> MPG123_SD90_B
+vlc_control_1 = trigger_filter >> VLC_BASE
 
 # Spotify
 spotify_control = [
@@ -30,17 +30,19 @@ mpk_soundcraft_control=Filter(CTRL|NOTE) >> [
 control_patch = PortSplit({
     midimix_midi : soundcraft_control,
     mpk_midi : ChannelSplit({
-        4 : pk5_mp3_control,
+        4 : mpg123_control_2,
     }),
     mpk_port_a : ChannelSplit({
          1 : CakewalkController,
-         8 : key_mp3_control,
-        12 : mpk_vlc_control,
+         8 : mpg123_control_1,
+         4 : mpg123_control_2,
+        12 : vlc_control_1,
         13 : p_hue,
-        #14 : spotify_control,
     }),
     mpk_port_b : ChannelSplit({
          1 : Program(sd90_port_a, EVENT_CHANNEL, EVENT_VALUE),
+         8 : mpg123_control_1,
+         4 : mpg123_control_2,
     }),
     sd90_midi_1 : Pass(),
     sd90_midi_2 : Pass(),
