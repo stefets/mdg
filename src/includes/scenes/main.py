8e7d7813348c7204e8a4c82b8129450193dea5c7
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-03-31 21:07:19 -0400

    feat: Add new scenes and MIDI patch for "Peace in Our Time"

--- src/includes/scenes/main.py
+++ src/includes/scenes/main.py
@@ -143,7 +143,7 @@ _scenes = {
             ),
             Scene("Fields of fire (bass)", init_patch=Call(GT1KPreset("U47-5")), patch=Discard()),
             Scene("Fields of fire (guit)", init_patch=Call(GT1KPreset("U09-2")), patch=Discard()),
-            Scene("PeaceInOurTime (bass)", init_patch=Call(GT1KPreset("U47-5")), patch=Discard()),
+            Scene("PeaceInOurTime (bass)", init_patch=Call(GT1KPreset("U47-5")), patch=p_peace_in_our_time),
             Scene("PeaceInOurTime (guit)", init_patch=Call(GT1KPreset("U09-1")), patch=Discard()),
         ],
     ),
