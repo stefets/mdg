2e9532ac0e20c49af8ee16bea18c7601487fc31f
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-09-14 18:28:04 -0400

    refactor(launch): Kill mpv on start and add comments

--- src/start_mpv.sh
+++ src/start_mpv.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Ensure that no other instances of mpv are running
+pkill mpv
+
+# Remove any existing socket files and log files
 rm -f /tmp/sd90a.sock /tmp/sd90b.sock /tmp/u192k.sock
 rm -f /tmp/mpv-sd90a.log /tmp/mpv-sd90b.log /tmp/mpv-u192k.log
 
+# Start mpv instances for each audio device with the specified options
 mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90a.log --msg-level=all=info  &
 mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90b.log --msg-level=all=info  &
 mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k --log-file=/tmp/mpv-u192k.log --msg-level=all=info &
