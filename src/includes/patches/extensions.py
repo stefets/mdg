9137b284ac1d0879d3f129da39d2fc8614fff45f
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-09-20 10:38:51 -0400

    refactor: Update MPV configuration and control mappings for audio and video devices

--- src/includes/patches/extensions.py
+++ src/includes/patches/extensions.py
@@ -7,11 +7,17 @@ Those modules are callable objects (__call__)
 terminal = TerminalUI()
 manager = PlaylistManager(terminal)
 
-# AUDIO_DEVICE multiple instances allow me to play sounds in parallal (dmix)
+# MPV adapters for audio and video devices, using the socket paths from the config
 mpv_config = config.get("mpv").get("socket")
 AUDIO_DEVICE_SD90_A = Call(MpvAdapter(mpv_config.get("SD90_A"), manager.playlist, terminal))
 AUDIO_DEVICE_SD90_B = Call(MpvAdapter(mpv_config.get("SD90_B"), manager.playlist, terminal))
-AUDIO_DEVICE_U192k  = Call(MpvAdapter(mpv_config.get("U192k"), manager.playlist, terminal))
+AUDIO_DEVICE_SD90_VIDEO  = Call(MpvAdapter(mpv_config.get("SD90_VIDEO"), manager.playlist, terminal))
+
+AUDIO_DEVICE_U192k_A  = Call(MpvAdapter(mpv_config.get("U192k_A"), manager.playlist, terminal))
+AUDIO_DEVICE_U192k_B  = Call(MpvAdapter(mpv_config.get("U192k_B"), manager.playlist, terminal))
+AUDIO_DEVICE_U192k_VIDEO  = Call(MpvAdapter(mpv_config.get("U192k_VIDEO"), manager.playlist, terminal))
+
+VIDEO = Call(MpvAdapter(mpv_config.get("VIDEO"), manager.playlist, terminal))
 
 # Playlist according to current scene, a singleton is enough
 PLAYLIST_MANAGER = Call(manager)
--- src/start_mpv.sh
+++ src/start_mpv.sh
@@ -4,10 +4,58 @@
 pkill mpv
 
 # Remove any existing socket files and log files
-rm -f /tmp/sd90a.sock /tmp/sd90b.sock /tmp/u192k.sock
-rm -f /tmp/mpv-sd90a.log /tmp/mpv-sd90b.log /tmp/mpv-u192k.log
+rm -f \
+    /tmp/sd90-a.sock \
+    /tmp/sd90-b.sock \
+    /tmp/sd90-video.sock \
+    /tmp/u192k-a.sock \
+    /tmp/u192k-b.sock \
+    /tmp/u192k-video.sock \
+    /tmp/video.sock
+rm -f \
+    /tmp/mpv-sd90-a.log \
+    /tmp/mpv-sd90-b.log \
+    /tmp/mpv-u192k-a.log \
+    /tmp/mpv-u192k-b.log \
+    /tmp/mpv-u192k-video.log \
+    /tmp/mpv-sd90-video.log \
+    /tmp/mpv-video.log
 
 # Start mpv instances for each audio device with the specified options
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90a.log --msg-level=all=info  &
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90b.log --msg-level=all=info  &
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k --log-file=/tmp/mpv-u192k.log --msg-level=all=info &
+
+# ---------------------------------- SD90 ----------------------------------
+# Audio only on device: SD90 (Socket: /tmp/sd90-a.sock)
+mpv --idle=yes --keep-open=no --no-video --no-terminal \
+    --input-ipc-server=/tmp/sd90-a.sock --audio-device=alsa/SD90 \
+    --log-file=/tmp/mpv-sd90-a.log --msg-level=all=info  &
+
+# Audio only on device: SD90 (Socket: /tmp/sd90-b.sock)
+mpv --idle=yes --keep-open=no --no-video --no-terminal \
+    --input-ipc-server=/tmp/sd90-b.sock --audio-device=alsa/SD90 \
+    --log-file=/tmp/mpv-sd90-b.log --msg-level=all=info  &
+
+# Video and audio on device: SD90 (Socket: /tmp/sd90-video.sock)
+mpv --idle=yes --keep-open=no --no-terminal \
+    --input-ipc-server=/tmp/sd90-video.sock --audio-device=alsa/SD90 \
+    --log-file=/tmp/mpv-sd90-video.log --msg-level=all=info  &
+
+# ---------------------------------- U192k ----------------------------------
+# Audio only on device: U192k (Socket: /tmp/u192k-a.sock)
+mpv --idle=yes --keep-open=no --no-video --no-terminal \
+    --input-ipc-server=/tmp/u192k-a.sock --audio-device=alsa/U192k \
+    --log-file=/tmp/mpv-u192k-a.log --msg-level=all=info &
+
+# Audio only on device: U192k (Socket: /tmp/u192k-b.sock)
+mpv --idle=yes --keep-open=no --no-video --no-terminal \
+    --input-ipc-server=/tmp/u192k-b.sock --audio-device=alsa/U192k \
+    --log-file=/tmp/mpv-u192k-b.log --msg-level=all=info &
+
+# Video and audio on device: U192k (Socket: /tmp/u192k-video.sock)
+mpv --idle=yes --keep-open=no --no-terminal \
+    --input-ipc-server=/tmp/u192k-video.sock --audio-device=alsa/U192k \
+    --log-file=/tmp/mpv-u192k-video.log --msg-level=all=info &
+
+# ---------------------------------- VIDEO ONLY ----------------------------------
+mpv --idle=yes --keep-open=no --no-terminal \
+    --input-ipc-server=/tmp/video.sock \
+    --ao=null &
